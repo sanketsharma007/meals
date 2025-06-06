9e453fce300e89117f7d5fae656949cdb2a673fc
Sanket Sharma <sanketsharma1870@gmail.com>
2025-06-06 11:50:18 +0530

    Adding cross screen navigation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget{
-
+class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+            (ctx) => MealsScreen(
+              title:
+                  'Category Title', // This should be replaced with the actual category title
+              meals:
+                  [], // This should be replaced with the actual meals for the selected category
+            ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
+      appBar: AppBar(title: const Text('Pick your category')),
       //HELPS TO RENDER GRID OF ITEMS
       body: GridView(
         padding: const EdgeInsets.all(25),
@@ -22,13 +35,16 @@ class CategoriesScreen extends StatelessWidget{
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children:[
+        children: [
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
             ),
-
-        ]),
+        ],
+      ),
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,41 +1,54 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals/models/meal.dart';
 
-class MealsScreen extends StatelessWidget{
-      const MealsScreen({
-      super.key,
-      required this.title,
-      required this.meals,
-    });
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
 
-
-    final String title;
-    final List<Meal> meals;
-
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(
+          meals[index].title,
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
-        title:  Text(title),
+        title: Text(title),
       ),
-      body: ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: (context, index) {
-          final meal = meals[index];
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(meal.imageUrl),
-            ),
-            title: Text(meal.title),
-            subtitle: Text('${meal.duration} min - ${meal.complexity.name}'),
-            onTap: () {
-              // Navigate to meal details screen
-            },
-          );
-        },
-      ),
+      body: content,
     );
   }
 }
